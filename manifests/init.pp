b6714da7734f6f0c517e8709533bfe5417e960f5
Tom Williams <tom.williams@hotschedules.com>
2016-05-16 15:31:04 -0400

    INFRA-52 - add hiera wrapper around puppetlabs-vcsrepo

---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+# vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2 foldmethod=marker
+
+class vcsrepo {
+
+  $defaults = {
+    'ensure'    => present,
+    'provider'  => git,
+  }
+  $repos = hiera_hash('vcsrepo', {})
+  validate_hash ( $repos )
+
+  create_resources(vcsrepo, $repos, $defaults)
+
+}
+
