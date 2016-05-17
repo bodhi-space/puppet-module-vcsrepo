8abaf188ff513d1d8ed79f5420325f9820ec7995
Tom Williams <tom.williams@hotschedules.com>
2016-05-16 22:08:10 -0400

    blah

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,42 +1,38 @@
 # vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2 foldmethod=marker
 
-class vcsrepos (
+class vcsrepo (
 
-#  $basic_auth_password  = undef,
-#  $basic_auth_username  = undef,
-#  $compression          = undef,
-#  $configuration        = undef,
-#  $conflict             = undef,
-#  $cvs_rsh              = undef,
-#  $depth                = undef,
-#  $ensure               = 'present',
-#  $excludes             = undef,
-#  $force                = undef,
-#  $fstype               = undef,
-#  $group                = undef,
-#  $identity             = undef,
-#  $module               = undef,
-#  $owner                = undef,
-#  $p4config             = undef,
-#  $path                 = undef,
-#  $provider             = 'git',
-#  $remote               = undef,
-#  $revision             = undef,
-#  $source               = undef,
-#  $submodules           = undef,
-#  $trust_server_cert    = undef,
-#  $user                 = undef,
+  $basic_auth_password  = undef,
+  $basic_auth_username  = undef,
+  $compression          = undef,
+  $configuration        = undef,
+  $conflict             = undef,
+  $cvs_rsh              = undef,
+  $depth                = undef,
+  $ensure               = 'present',
+  $excludes             = undef,
+  $force                = undef,
+  $fstype               = undef,
+  $group                = undef,
+  $identity             = undef,
+  $module               = undef,
+  $owner                = undef,
+  $p4config             = undef,
+  $path                 = undef,
+  $provider             = 'git',
+  $remote               = undef,
+  $revision             = undef,
+  $source               = undef,
+  $submodules           = undef,
+  $trust_server_cert    = undef,
+  $user                 = undef,
 
 ) {
 
-  $defaults = {
-    provider  = 'git',
-    ensure    = 'present',
-  }
   $repos = hiera_hash('vcsrepo', {})
   validate_hash ( $repos )
 
-  create_resources(vcsrepo, $repos, $defaults)
+  create_resources(vcsrepo, $repos)
 
 }
 
