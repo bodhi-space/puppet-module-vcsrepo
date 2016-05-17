5a02d48d88ff23321df7adc5ab635a5d66f611e3
Tom Williams <tom.williams@hotschedules.com>
2016-05-16 22:03:35 -0400

    INFRA-52 - 2l3k4j32

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,42 @@
 # vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2 foldmethod=marker
 
-class vcsrepo {
+class vcsrepos (
 
+#  $basic_auth_password  = undef,
+#  $basic_auth_username  = undef,
+#  $compression          = undef,
+#  $configuration        = undef,
+#  $conflict             = undef,
+#  $cvs_rsh              = undef,
+#  $depth                = undef,
+#  $ensure               = 'present',
+#  $excludes             = undef,
+#  $force                = undef,
+#  $fstype               = undef,
+#  $group                = undef,
+#  $identity             = undef,
+#  $module               = undef,
+#  $owner                = undef,
+#  $p4config             = undef,
+#  $path                 = undef,
+#  $provider             = 'git',
+#  $remote               = undef,
+#  $revision             = undef,
+#  $source               = undef,
+#  $submodules           = undef,
+#  $trust_server_cert    = undef,
+#  $user                 = undef,
+
+) {
+
+  $defaults = {
+    provider  = 'git',
+    ensure    = 'present',
+  }
   $repos = hiera_hash('vcsrepo', {})
   validate_hash ( $repos )
 
-  create_resources(vcsrepo, $repos)
+  create_resources(vcsrepo, $repos, $defaults)
 
 }
 
